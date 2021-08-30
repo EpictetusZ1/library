189f6cd2e97daa1062509e9e1445982296cf7258
J Heaton <jackrobertheaton@gmail.com>
2021-08-30 11:26:20 -0400

    Add display book data function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let myLibrary = []
+// Test books for Library display formatting
+const book1 = new Book("The Obstacle is the Way", "Ryan Holiday", 391, "Read")
+const book2 = new Book("The Witcher", "Andrej Sapkowski", 400, "Read")
+const book3 = new Book("The Bible", "Various", 1200, "Not Read")
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -12,6 +15,9 @@ Book.prototype.getInfo = function () {
 }
 
 const addBookForm = document.getElementById("bookForm")
+const library = document.getElementById("library")
+
+let myLibrary = [book1, book2, book3]
 
 function addBookToLibrary(e) {
     e.preventDefault()
@@ -20,8 +26,21 @@ function addBookToLibrary(e) {
     let newBook = new Book(...testData.values())
     myLibrary.push(newBook)
     addBookForm.reset()
+    displayBook()
 }
 
-addBookForm.addEventListener("submit", (e) => {
-    addBookToLibrary(e)
-})
+addBookForm.addEventListener("submit", (e) => addBookToLibrary(e))
+
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; i ++) {
+        let bookDiv = document.createElement("div")
+        let title = document.createElement("h3")
+        let author = document.createElement("h4")
+        let paragraph = document.createElement("p")
+        library.appendChild(bookDiv).classList.add("book")
+        bookDiv.appendChild(title).textContent = myLibrary[i].title
+        bookDiv.appendChild(author).textContent = "By: \r\n" + myLibrary[i].author
+        bookDiv.appendChild(paragraph).textContent = myLibrary[i].pages
+        bookDiv.appendChild(paragraph).textContent = myLibrary[i].read
+    }
+}
